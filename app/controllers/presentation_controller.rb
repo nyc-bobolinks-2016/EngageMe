3ee013c44cd98f88a5f307d5334944540aee5399
Amanda Hinchey <email": null>
2016-11-04 11:33:04 -0400

    Add new user functionality

---
+++ app/controllers/presentation_controller.rb
@@ -0,0 +1,2 @@
+class PresentationController < ApplicationController
+end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,26 @@
 class UsersController < ApplicationController
 
-  def index
+  def new
   end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      session[:user_id] = @user.id
+      redirect_to user_path(@user)
+    else
+      render 'new'
+    end
+  end
+
+  def show
+    @user = User.find_by(id: params[:id])
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:username, :email, :organization, :password, :password_confirmation)
+  end
+
 end
