21732f0aac2b8e5375dd828e9c61a7e2916a1744
Amanda Hinchey <email": null>
2016-11-04 15:22:32 -0400

    Continue working on new presentation controller/form

--- app/controllers/presentations_controller.rb
+++ app/controllers/presentations_controller.rb
@@ -4,4 +4,27 @@ class PresentationsController < ApplicationController
     @presentation = Presentation.find(params[:id])
   end
 
+  def new
+    @user = User.find(params[:user_id])
+  end
+
+  def create
+    @user = User.find(params[:user_id])
+    @presentation = @user.presentations.create(presentation_params)
+    byebug
+    if @presentation.save
+      redirect_to user_presentation_path(@user, @presentation)
+    else
+      @errors = @presentation.errors.full_messages
+      render 'new'
+    end
+  end
+
+  private
+
+  def presentation_params
+    params.require(:presentation).permit(:name, :location, :audience, :start_time, :end_time, :notes)
+  end
+
+
 end
