80e606ca3fb4e466aadfd7838761d21e38a302c4
Tyler Sharp <tyler.v.sharp@gmail.com>
2016-11-05 16:04:16 -0400

    Switched API.

--- Gemfile
+++ Gemfile
@@ -14,10 +14,6 @@ gem 'uglifier', '>= 1.3.0'
 # Use CoffeeScript for .coffee assets and views
 gem 'coffee-rails', '~> 4.2'
 # See https://github.com/rails/execjs#readme for more supported runtimes
-# gem 'therubyracer', platforms: :ruby
-
-# Call RESTful API
-gem 'unirest'
 # Use jquery as the JavaScript library
 gem 'jquery-rails'
 # Build JSON APIs with ease. Read more: https://github.com/rails/jbuilder
--- app/services/get_emotions.rb
+++ app/services/get_emotions.rb
@@ -1,25 +1,23 @@
-require 'unirest'
+require 'net/http'
 
 class GetEmotions
   def initialize
-    @url = "http://upload.wikimedia.org/wikipedia/commons/thumb/8/8e/Batsheva_theater_crowd_in_Tel_Aviv_by_David_Shankbone.jpg/600px-Batsheva_theater_crowd_in_Tel_Aviv_by_David_Shankbone.jpg"
+    @url = "https://dbc-engage-me.herokuapp.com/snapshot.jpg"
   end
 
   def call
-    response = Unirest.post "https://api.projectoxford.ai/emotion/v1.0/recognize",
-                            headers:{ "Content-Type" => "application/json", "Ocp-Apim-Subscription-Key" => Rails.application.secrets.ms_subscription_key },
-                            { "url": @url }
-    # RestClient::Resource.new('https://api.projectoxford.ai/emotion/v1.0/recognize')
-    # response = client.post { "url": 'https://dbc-engage-me.herokuapp.com/snapshot.jpg' }, :content_type => 'application/json', :'Ocp-Apim-Subscription-Key' => Rails.application.secrets.ms_subscription_key
-    # RestClient.post('https://api.projectoxford.ai/emotion/v1.0/recognize', { :payload => { "url": @url } }, { :headers => { :Content_Type => application/json, :Ocp-Apim-Subscription-Key => Rails.application.secrets.ms_subscription_key } }
-    # ::Request.execute(method: :post, url: 'https://api.projectoxford.ai/emotion/v1.0/recognize', payload: { "url": @url }, headers: { Content_Type: application/json, Ocp-Apim-Subscription-Key: Rails.application.secrets.ms_subscription_key })
-    # post 'https://api.projectoxford.ai/emotion/v1.0/recognize', {content_type :json, accept: :json}, {
-    #   "url": "https://dbc-engage-me.herokuapp.com/snapshot.jpg" }, headers={"Ocp-Apim-Subscription-Key" => Rails.application.secrets.ms_subscription_key, })
-    # json = open("https://api.projectoxford.ai/emotion/v1.0/recognize?url=#{@url}",
-    # "Ocp-Apim-Subscription-Key" => "Key").read
-    # uri = "https://api.projectoxford.ai/emotion/v1.0/recognize?url=#{@url}"
-    # Net::HTTP.start(host, 3000) do |http|
-    #   request = Net::HTTP::Get.new uri
-    #   response = http.request request
+    uri = URI('https://api.projectoxford.ai/emotion/v1.0/recognize')
+    uri.query = URI.encode_www_form({
+      })
+    request = Net::HTTP::Post.new(uri.request_uri)
+    # Request headers
+    request['Content-Type'] = 'application/json'
+    # Request headers
+    request['Ocp-Apim-Subscription-Key'] = Rails.application.secrets.ms_subscription_key
+    # Request body
+    request.body = { "url": @url }.to_json
+
+    response = Net::HTTP.start(uri.host, uri.port, :use_ssl => uri.scheme == 'https') do |http|
+        http.request(request)
   end
 end
