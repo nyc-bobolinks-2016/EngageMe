e2e86cf1d76ed5d89fd36319889d58bbed77fad3
Tyler Sharp <tyler.v.sharp@gmail.com>
2016-11-05 14:37:02 -0400

    Edit API settings to try again.

--- app/services/get_emotions.rb
+++ app/services/get_emotions.rb
@@ -6,7 +6,8 @@ class GetEmotions
   end
 
   def call
-    RestClient::Request.execute(method: :post, url: 'https://api.projectoxford.ai/emotion/v1.0/recognize', payload: { "url": @url }, headers: { Content_Type: application/json, Ocp-Apim-Subscription-Key: Rails.application.secrets.ms_subscription_key })
+    RestClient.post 'https://api.projectoxford.ai/emotion/v1.0/recognize', { "url": @url }, { headers: { Content_Type: application/json, Ocp-Apim-Subscription-Key: Rails.application.secrets.ms_subscription_key } }
+    # ::Request.execute(method: :post, url: 'https://api.projectoxford.ai/emotion/v1.0/recognize', payload: { "url": @url }, headers: { Content_Type: application/json, Ocp-Apim-Subscription-Key: Rails.application.secrets.ms_subscription_key })
     # post 'https://api.projectoxford.ai/emotion/v1.0/recognize', {content_type :json, accept: :json}, {
     #   "url": "https://dbc-engage-me.herokuapp.com/snapshot.jpg" }, headers={"Ocp-Apim-Subscription-Key" => Rails.application.secrets.ms_subscription_key, })
     # json = open("https://api.projectoxford.ai/emotion/v1.0/recognize?url=#{@url}",
