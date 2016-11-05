9c11fa0e40a1e462d740a797465151ac53090a56
Roman Levytskyy <email": null>
2016-11-05 16:15:49 -0400

    build a service class to work with api respose

--- app/services/get_emotions.rb
+++ app/services/get_emotions.rb
@@ -10,11 +10,8 @@ class GetEmotions
     uri.query = URI.encode_www_form({
       })
     request = Net::HTTP::Post.new(uri.request_uri)
-    # Request headers
     request['Content-Type'] = 'application/json'
-    # Request headers
     request['Ocp-Apim-Subscription-Key'] = Rails.application.secrets.ms_subscription_key
-    # Request body
     request.body = { "url": @url }.to_json
 
     response = Net::HTTP.start(uri.host, uri.port, :use_ssl => uri.scheme == 'https') do |http|
