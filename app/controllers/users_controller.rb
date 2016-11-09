069006251e6a991778d9f9f70b21ca3ae984671b
Tyler Sharp <tyler.v.sharp@gmail.com>
2016-11-09 15:29:06 -0500

    Set up welcome page to show only to new users.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -13,7 +13,7 @@ class UsersController < ApplicationController
     @user = User.new(user_params)
     if @user.save
       session[:user_id] = @user.id
-      redirect_to user_path(@user)
+      redirect_to welcome_user_path(@user)
     else
       @errors = @user.errors.full_messages
       render 'new'
