97b81a0ba6558c9ad38f96156fef722897ab5770
Roman Levytskyy <email": null>
2016-11-11 13:15:17 -0500

    change resume and pause button text to uppercase, center label on bar chart

--- app/assets/javascripts/camera.js
+++ app/assets/javascripts/camera.js
@@ -53,12 +53,12 @@ $('#run-container').ready(function(){
     if(presentation){
       timerTime = $('#clock').data('seconds');
       $('#clock').timer('remove');
-      $('#start').attr('value', 'resume');
+      $('#start').attr('value', 'Resume');
       $('#faces').hide();
       presentation = false
     } else {
 
-      $('#start').attr('value', 'pause');
+      $('#start').attr('value', 'Pause');
       $('#faces').show().text("Detecting Faces...");
       $('#clock').timer({
         format: '%H:%M:%S',
